9e29326a3a113375c26fb459edcf71e335f0331c
paultranvan <paul@cozycloud.cc>
2021-05-25 18:56:52 +0200

    feat: Add env in config template

--- config.py
+++ config.py
@@ -1,17 +1,22 @@
 import yaml
 import sys
-
+import os
 
 class Config(object):
-
     try:
         with open("config.yml", "r") as config:
             cfg = yaml.load(config, Loader=yaml.FullLoader)
-            db = cfg["database"]
+            
+            env = cfg["env"]
+            if os.environ.get("TESTING"):
+                env = "test"
+            db = cfg["envs"][env]["database"]
             SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@{}:{}/{}".format(
                 db["user"], db["password"], db["host"], db["port"], db["name"])
             SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+
     except Exception as err:
         print("Error while opening config.yaml: " + repr(err))
         sys.exit()
+
