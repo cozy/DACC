510a52196d8967a0050e8b2b58f173408eeb4625
paultranvan <paul@cozycloud.cc>
2021-05-27 17:58:59 +0200

    fix: MeasuresDefinition fixtures

--- dacc/fixtures/measures_definition.sql
+++ dacc/fixtures/measures_definition.sql
@@ -1,4 +1,3 @@
-insert into measures_definition(name, org, created_by, aggregation_period, group1_key) values('connection-count-daily', 'grandlyon', 'ecolyo', 'day', 'device')
-insert into measures_definition(name, org, created_by, aggregation_period, group1_key, group2_key, group3_key) values('konnector-event-daily', 'grandlyon', 'ecolyo', 'day', 'slug', 'event_type', 'status')
-insert into measures_definition(name, org, created_by, aggregation_period, group1_key, group2_key, group3_key) values('energy-consumption-daily', 'grandlyon', 'ecolyo', 'day', 'energy_type')
-
+insert into measures_definition(name, org, created_by, aggregation_period, group1_key) values('connection-count-daily', 'grandlyon', 'ecolyo', 'day', 'device');
+insert into measures_definition(name, org, created_by, aggregation_period, group1_key, group2_key, group3_key) values('konnector-event-daily', 'grandlyon', 'ecolyo', 'day', 'slug', 'event_type', 'status');
+insert into measures_definition(name, org, created_by, aggregation_period, group1_key) values('energy-consumption-daily', 'grandlyon', 'ecolyo', 'day', 'energy_type');
