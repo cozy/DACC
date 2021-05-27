6368148d23bf6523e844cb40147a68c8e88a02bc
Sébastien Blaisot <sebastien.blaisot@cozycloud.cc>
2021-05-27 17:40:12 +0200

    chore: Add versionning

--- dacc/__init__.py
+++ dacc/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from config import Config
+from dacc.version import __version__
 from flask_sqlalchemy import SQLAlchemy
 
 
 configdata = Config()
 dacc = Flask(__name__)
 dacc.config.from_object(configdata)
+dacc.version = __version__
 
 db = SQLAlchemy(dacc)
 
