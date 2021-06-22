2cf346b084e50d837543ada127b2820c3c6ec770
paultranvan <paul@cozycloud.cc>
2021-06-22 18:19:12 +0200

    feat: Add cli to insert JSON measures definitions
    We add this command and deprecate the SQL insertion command because it
    is way easier to express and deal with definitions updates.
    
    fix: JSON definition insertion

--- dacc/insertion.py
+++ dacc/insertion.py
@@ -40,14 +40,19 @@ def insert_raw_measure(measure):
 def insert_measure_definition(definition):
     try:
         d = MeasureDefinition(
+            id=definition.get("id"),
             name=definition.get("name"),
+            org=definition.get("org"),
             created_by=definition.get("createdBy"),
-            description=definition.get("description"),
-            aggregation_period=definition.get("aggregationPeriod"),
-            contribution_threshold=definition.get("contributionThreshold"),
             group1_key=definition.get("group1Key"),
             group2_key=definition.get("group2Key"),
             group3_key=definition.get("group3Key"),
+            description=definition.get("description"),
+            aggregation_period=definition.get("aggregationPeriod"),
+            execution_frequency=definition.get("executionFrequency"),
+            aggregation_threshold=definition.get("aggregationThreshold"),
+            access_app=definition.get("accessApp"),
+            access_public=definition.get("accessPublic"),
         )
         db.session.add(d)
         db.session.commit()
